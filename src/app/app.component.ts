c56a18ea43a77350511814d6a69d78ca7c53a67f
EvandroMonteiro <goodvandro@gmail.com>
2020-07-22 12:50:38 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reactive-form';
+
+  SchoolDetailsForm: FormGroup;
+
+  AllClassData = [
+    {
+      'className': '5th'
+    },
+    {
+      'className': '8th'
+    },
+    {
+      'className': '10th'
+    },
+    {
+      'className': '12th'
+    },
+  ]
+  constructor(private fb: FormBuilder) {
+
+  }
+  ngOnInit() {
+    this.createform();
+  }
+
+  createform() {
+    let arr = [];
+    for (let i = 0; i < this.AllClassData.length; i++) {
+      arr.push(this.BuildFormDynamic(this.AllClassData[i]))
+    }
+
+    this.SchoolDetailsForm = this.fb.group({
+      SchoolName: [''],
+      ClassDetails: this.fb.array(arr)
+    })
+  }
+
+  BuildFormDynamic(product): FormGroup {
+    return this.fb.group({
+      Class: [product.className],
+      TotalStudent: [''],
+      GradeAStudent: [''],
+      GradeBStudent: ['']
+    })
+  }
+
+  SaveData() {
+    console.log(this.SchoolDetailsForm.value);
+    //pass this data to service and api node/webapi
+  }
+
 }
